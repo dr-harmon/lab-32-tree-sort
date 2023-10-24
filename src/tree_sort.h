c5387bac4a6749e28c5f8166fd8d6edc69b5d408
Trevor Harmon <tharmon@laspositascollege.edu>
2023-10-23 17:21:17 -0700

    Add solution

--- src/tree_sort.h
+++ src/tree_sort.h
@@ -1,9 +1,20 @@
 #pragma once
 
+#include <map>
 #include <vector>
 
 template <typename E>
 void treeSort(std::vector<E>& v)
 {
-    // TODO
+    std::map<E, E> map;
+
+    for (const auto& e : v) {
+        map[e] = e;
+    }
+
+    v.clear();
+
+    for (const auto& pair : map) {
+        v.push_back(pair.first);
+    }
 }
